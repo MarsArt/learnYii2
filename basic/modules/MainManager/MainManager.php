50dc4e017f3411b5265cc1127e48ca2067969b5d
MarsArt <MarsArt@yandex.ru>
2014-07-07 23:01:40 -0400

    initial project

---
+++ basic/modules/MainManager/MainManager.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\MainManager;
+
+class MainManager extends \yii\base\Module
+{
+    public $controllerNamespace = 'app\modules\MainManager\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
